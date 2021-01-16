505b1aceea381795ba7965a370aa37ad2d1a7c57
andyyou <andyyu0920@gmail.com>
2021-01-16 12:59:13 +0800

    Install Bootstrap 5 beta

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -10,6 +10,7 @@ window.axios = require('axios');
 
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 
+require('bootstrap');
 /**
  * Echo exposes an expressive API for subscribing to channels and listening
  * for events that are broadcast by Laravel. Echo and event broadcasting
