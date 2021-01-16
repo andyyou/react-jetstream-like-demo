9fed30f0b304ac90b157df3ca9064d8b48a1970f
andyyou <andyyu0920@gmail.com>
2021-01-16 12:34:40 +0800

    Inertia - Server Side

---
+++ resources/views/app.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+  <!-- Styles -->
+  <link rel="stylesheet" href="{{ mix('css/app.css') }}">
+
+  <!-- Scripts -->
+  <script src="{{ mix('js/app.js') }}" defer></script>
+  <title>{{ config('app.name', 'Demo') }}</title>
+</head>
+<body>
+  @inertia
+</body>
+</html>
